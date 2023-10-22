7dd1f252e4e313ccab0fcc6ff07a74c6cfb4c442
Fetralht <lavex2002@gmail.com>
2023-10-22 21:24:10 +0300

    MAJ1 tuto js

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ const express=require('express')
 
 const app=express()
 
-app.use((req,res,next)=>{
-   console.log('it s middleware')
-   res.send('<h1>express.js from nodejs</h1>')
+app.use('/add-product',(req,res,next)=>{
+   console.log('In new middleware')
+   res.send('<h1>add "the add-product" </h1>')
    next()
 })
-app.use((req,res,next)=>{
-    console.log('it s middleware2222')
-    res.setHeader('Content-Type','text/html')
-    res.send('<h1>express.js  nodejs</h1>')
+app.use('/',(req,res,next)=>{
+    console.log('In another middleware')
+    res.send('<h1>helo from express</h1>')
  })
 app.listen(2001)
 
